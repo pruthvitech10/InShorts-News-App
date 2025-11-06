c798f69b4f71cb150520f7bc576941d3dab4f6fc
Pruthvirajsinh Punada <pruthviraj1022004@gmail.com>
2025-11-06 08:17:59 +0100

    Initial Commit

---
+++ Newssss/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Newssss
+//
+//  Created by pruthvirajsinh punada on 06/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Newssss/NewssssApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewssssApp.swift
+//  Newssss
+//
+//  Created by pruthvirajsinh punada on 06/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NewssssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
